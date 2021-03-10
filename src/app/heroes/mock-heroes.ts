ddaa8997361e16d8d3e8b2cab5122e01abcb040c
Anthony Lynn <pendreamsyouth@gmail.com>
2021-03-10 15:43:46 +0200

    First Commit

---
+++ src/app/heroes/mock-heroes.ts
@@ -0,0 +1,10 @@
+import {Hero} from "./hero";
+
+export const HEROES: Hero[] = [
+  {id: 11, name: "DR fuckyou"},
+  {id: 14, name: "Bombasto"},
+  {id: 16, name: "Mr Wonderful"},
+  {id: 15, name: "blahblah"}
+];
+
+
